df1d4e13799d59117cf11994d38ebff6eab64021
Prathamesh <ov01809@gmail.com>
2024-09-25 22:18:21 +0530

    First Commit

---
+++ Task-Project/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+import { headerComponent } from './header/header.component';
+import { UserComponent } from "./user/user.component";
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [headerComponent, UserComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {}
---
+++ Task-Project/src/app/header/header.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector:'app-header',
+    standalone:true,
+    // template:'<h1>hello Word</h1>'
+    templateUrl:'./header.component.html',
+    styleUrl:'./header.component.css'
+})
+export class headerComponent{
+
+}
